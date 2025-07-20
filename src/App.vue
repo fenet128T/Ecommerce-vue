8257ed7ada8d091a8567da571f81a1b3ceb7f0a4
fenet128t <fenet128t@gmail.com>
2025-07-21 02:06:34 +0300

    Added About

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <Products />
     <Categories />
     <Offers />
+    <About />
  </div>
 </template>
 
@@ -14,4 +15,5 @@ import Hero from './components/Hero.vue'
 import Products from './components/Products.vue'
 import Categories from './components/Categories.vue'
 import Offers from './components/Offers.vue'
+import About from './components/About.vue'
 </script>
